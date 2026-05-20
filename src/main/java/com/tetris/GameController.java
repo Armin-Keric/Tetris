0efd866c67647216f7c8f7efb48c060d374fc7f6
LeBonsBay <sasuek94@gmail.com>
2026-05-20 20:52:02 +0200

    feat: groundwork for movement, refactored a little bit, added randomness of tetromino creation in the initialize() method

---
+++ src/main/java/com/tetris/GameController.java
@@ -0,0 +1,58 @@
+package com.tetris;
+
+import com.tetris.shapes.*;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
+
+import java.net.URL;
+import java.util.*;
+
+
+public class GameController implements Initializable {
+    public Pane gamePane;
+    public Pane gameFieldPane;
+    public Pane nextBlockPane;
+    public Label currentScoreLabel;
+    public Label highscoreLabel;
+    public Label currentLevelLabel;
+    public Label linesLabel;
+
+    private Random random = new Random();
+    private ArrayList<Shape> arrayList = new ArrayList<>(Arrays.asList(new I(15, 10, 20),
+            new J(15, 10, 20),
+            new L(15, 10, 20),
+            new O(15, 10, 20),
+            new S(15, 10, 20),
+            new T(15, 10, 20),
+            new Z(15, 10, 20)));
+    //Everything inherits from Shape so we can just use Shape here (looks better than Object too
+    private Shape form = arrayList.get(random.nextInt(arrayList.size()));
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        gamePane.setFocusTraversable(true);
+        gamePane.requestFocus();
+        gamePane.getChildren().add((form));
+
+    }
+
+    public void onKeyPressed(KeyEvent keyEvent) {
+
+
+        switch (keyEvent.getCode()) {
+            case Q -> form.rotateLeft(form);
+            case E -> form.rotateRight(form);
+            case S -> form.moveDown(form);
+            case A -> form.moveLeft(form);
+            case D -> form.moveRight(form);
+            case SPACE -> form.hardDrop(form);
+        }
+    }
+
+
+    public ArrayList<Shape> getArrayList() {
+        return arrayList;
+    }
+}
--- src/main/java/com/tetris/shapes/Shape.java
+++ src/main/java/com/tetris/shapes/Shape.java
@@ -6,17 +6,64 @@ import javafx.scene.Group;
 import javax.swing.text.GapContent;
 
 public abstract class Shape extends Group {
-    private int maxWidth=0;
-    private int maxHeight=0;
-    private int blocksize=0;
-    public Block[] blocks=new  Block[4];
-    public Shape(int blocksize, int maxWidth, int maxHeight){
-        this.blocksize=blocksize;
-        this.maxHeight=maxHeight;
-        this.maxWidth=maxWidth;
+    private int maxWidth = 0;
+    private int maxHeight = 0;
+    private int blocksize = 0;
+    public Block[] blocks = new Block[4];
+
+    public Shape(int blocksize, int maxWidth, int maxHeight) {
+        this.blocksize = blocksize;
+        this.maxHeight = maxHeight;
+        this.maxWidth = maxWidth;
     }
-    public Block[] getBlocks(){
+
+    public Block[] getBlocks() {
         return this.blocks;
+    }
+
+
+    //A
+    public void moveLeft(Shape shape) {
+
+        if (shape instanceof I) {
+
+        } else if (shape instanceof J) {
+
+        } else if (shape instanceof L) {
+
+        } else if (shape instanceof O) {
+
+        } else if (shape instanceof S) {
+            
+        } else if (shape instanceof  T) {
+            
+        } else if (shape instanceof Z) {
+            
+        }
+
+    }
+
+    public void rotateLeft(Shape shape) {
+
+    }
+
+    //D
+    public void moveRight(Shape shape) {
+
+    }
+
+    public void rotateRight(Shape shape) {
+
+    }
+
+    //This is the SoftDrop too...
+    public void moveDown(Shape shape) {
+
+    }
+
+    //SPACE
+    public void hardDrop(Shape shape) {
+
     }
 
 }
