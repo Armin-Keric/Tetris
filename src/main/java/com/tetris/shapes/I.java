de9679e65d2d33fde158d3db8d4c34d2844f95ad
LeBonsBay <armin.keric0001@gmail.com>
2026-05-29 15:55:33 +0200

    chore: fix of formatting in the Shape classes

--- src/main/java/com/tetris/shapes/I.java
+++ src/main/java/com/tetris/shapes/I.java
@@ -2,19 +2,19 @@ package com.tetris.shapes;
 
 import javafx.scene.paint.Color;
 
-public class I extends Shape{
-    public I(int blockSize, int maxWidth, int maxHeight){
-        super(blockSize,maxWidth,maxHeight);
-        this.blocks[0]=new Block(blockSize);
-        this.blocks[0].setPos(new Position(maxWidth/2-1,0));
-        this.blocks[1]=new Block(blockSize);
-        this.blocks[1].setPos(new Position(maxWidth/2-1,1));
-        this.blocks[2]=new Block(blockSize);
-        this.blocks[2].setPos(new Position(maxWidth/2-1,2));
-        this.blocks[3]=new Block(blockSize);
-        this.blocks[3].setPos(new Position(maxWidth/2-1,3));
+public class I extends Shape {
+    public I(int blockSize, int maxWidth, int maxHeight) {
+        super(blockSize, maxWidth, maxHeight);
+        this.blocks[0] = new Block(blockSize);
+        this.blocks[0].setPos(new Position(maxWidth / 2 - 1, 0));
+        this.blocks[1] = new Block(blockSize);
+        this.blocks[1].setPos(new Position(maxWidth / 2 - 1, 1));
+        this.blocks[2] = new Block(blockSize);
+        this.blocks[2].setPos(new Position(maxWidth / 2 - 1, 2));
+        this.blocks[3] = new Block(blockSize);
+        this.blocks[3].setPos(new Position(maxWidth / 2 - 1, 3));
 
-        for (Block block:blocks){
+        for (Block block : blocks) {
             this.getChildren().add(block);
         }
     }
--- src/main/java/com/tetris/shapes/J.java
+++ src/main/java/com/tetris/shapes/J.java
@@ -2,19 +2,19 @@ package com.tetris.shapes;
 
 import javafx.scene.paint.Color;
 
-public class J extends Shape{
-    public J(int blockSize, int maxWidth, int maxHeight){
-        super(blockSize,maxWidth,maxHeight);
-        this.blocks[0]=new Block(blockSize);
-        this.blocks[0].setPos(new Position(maxWidth/2-1,0));
-        this.blocks[1]=new Block(blockSize);
-        this.blocks[1].setPos(new Position(maxWidth/2-1,1));
-        this.blocks[2]=new Block(blockSize);
-        this.blocks[2].setPos(new Position(maxWidth/2-1,2));
-        this.blocks[3]=new Block(blockSize);
-        this.blocks[3].setPos(new Position(maxWidth/2-2,2));
+public class J extends Shape {
+    public J(int blockSize, int maxWidth, int maxHeight) {
+        super(blockSize, maxWidth, maxHeight);
+        this.blocks[0] = new Block(blockSize);
+        this.blocks[0].setPos(new Position(maxWidth / 2 - 1, 0));
+        this.blocks[1] = new Block(blockSize);
+        this.blocks[1].setPos(new Position(maxWidth / 2 - 1, 1));
+        this.blocks[2] = new Block(blockSize);
+        this.blocks[2].setPos(new Position(maxWidth / 2 - 1, 2));
+        this.blocks[3] = new Block(blockSize);
+        this.blocks[3].setPos(new Position(maxWidth / 2 - 2, 2));
 
-        for (Block block:blocks){
+        for (Block block : blocks) {
             this.getChildren().add(block);
         }
 
